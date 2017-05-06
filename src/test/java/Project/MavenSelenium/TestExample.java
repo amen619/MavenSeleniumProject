691961df583e4fb4fa9c3ef617da6c7fb04a2318
joudaon <jonuda77@gmail.com>
2017-05-06 03:55:07 +0200

    New TestClass added

---
+++ src/test/java/Project/MavenSelenium/TestExample.java
@@ -0,0 +1,16 @@
+package Project.MavenSelenium;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class TestExample {
+
+	@Test
+	public void beforeClass(){
+		System.out.println("Holaaaaaaa");
+		assertTrue( true );
+	}
+	
+	
+}
