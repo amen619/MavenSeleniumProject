79500092f84fc08a22c6215d98dbcfbb7d17f096
joudaon <jonuda77@gmail.com>
2017-05-06 04:06:11 +0200

    New test case added

--- src/test/java/Project/MavenSelenium/TestExample.java
+++ src/test/java/Project/MavenSelenium/TestExample.java
@@ -7,9 +7,15 @@ import org.testng.annotations.Test;
 public class TestExample {
 
 	@Test
-	public void beforeClass(){
+	public void Test1(){
 		System.out.println("Holaaaaaaa");
 		assertTrue( true );
+	}
+	
+	@Test
+	public void Test2(){
+		System.out.println("Holaaaaaaa test 2");
+		assertTrue( false );
 	}
 	
 	
